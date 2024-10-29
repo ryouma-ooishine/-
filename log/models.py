6c199d1404246ec9f31c0101f5779959aba3d7f9
ryouma <ryouma7337@icloud.com>
2024-10-29 13:57:15 +0900

    add DB

--- log/models.py
+++ log/models.py
@@ -1,3 +1,14 @@
 from django.db import models
-
+import uuid
 # Create your models here.
+
+class Page(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    company_name = models.CharField(max_length=100)
+    buy_price = models.BigIntegerField()
+    buy_shares = models.BigAutoField()
+    sell_price = models.BigIntegerField()
+    sell_shares = models.BigAutoField()
+    buy_day = models.DateField()
+    sell_day = models.DateField()
+
--- log/views.py
+++ log/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render # type: ignore
 from django.views import View # type: ignore
+from datetime import datetime
+from zoneinfo import ZoneInfo
+
+
 # Create your views here.
 class IndexView(View):
     def get(self, request):
-        return render(request, "log/index.html")
-    
+        datetime_now = datetime.now(
+            ZoneInfo("Asia/Tokyo")
+        ).strftime("%Y年%月%d日 %H:%M:%S")
+        return render(
+            request, "log/index.html",{"datetime_now":datetime_now})
 index = IndexView.as_view()
