cfbf51b2b7308b7f3cd528facea48196dca42755
ryouma <ryouma7337@icloud.com>
2024-11-04 14:40:42 +0900

    CSS

--- log/forms.py
+++ log/forms.py
@@ -1,7 +1,15 @@
 from django.forms import ModelForm
 from .models import Page
+from django import forms
 
 class PageForm(ModelForm):
     class Meta:
         model = Page
-        fields = ['company_name', 'buy_day', 'buy_price', 'buy_shares']
+        fields = ['company_name', 'buy_day', 'buy_price', 'buy_shares','buy_reason']
+        widgets = {
+            'buy_reason': forms.Textarea(attrs={
+                'placeholder': '購入理由を入力してください...',
+                'rows': 6,  
+                'style': 'width: 100%;' 
+            })
+        }
--- log/views.py
+++ log/views.py
@@ -1,12 +1,11 @@
-from django.shortcuts import render,redirect # type: ignore
-from django.views import View # type: ignore
+from django.shortcuts import render,redirect 
+from django.views import View 
 from datetime import datetime
 import pytz
 from .models import Page
 from .forms import PageForm
 from django.views.generic import ListView
 
-# Create your views here.
 class IndexView(View):
     def get(self, request):
         datetime_now = datetime.now(
@@ -30,16 +29,14 @@ class PageCreateView(View):
 class PageListView(ListView):
     model = Page
     def get_queryset(self):
-        # Pageオブジェクトを取得
         queryset = super().get_queryset()
         for page in queryset:
-            page.total_amount = page.buy_price * page.buy_shares  # 総額を計算
+            page.total_amount = page.buy_price * page.buy_shares 
         return queryset
 
     def get_context_data(self, **kwargs):
-        # コンテキストに追加データを追加
         context = super().get_context_data(**kwargs)
-        context['page_list'] = self.get_queryset()  # 計算されたオブジェクトのリストを渡す
+        context['page_list'] = self.get_queryset()
         return context
 
 
